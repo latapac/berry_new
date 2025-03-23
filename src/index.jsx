f3471ea34bad83cf421013c16b06ec82c84f2df2
latapac <latashree@gmail.com>
2025-03-23 11:38:28 +0530

    remove web vitals

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,6 @@ import { createRoot } from 'react-dom/client';
 
 // project imports
 import App from 'App';
-import * as serviceWorker from 'serviceWorker';
-import reportWebVitals from 'reportWebVitals';
 import { ConfigProvider } from 'contexts/ConfigContext';
 import './main.css';
 // style + assets
@@ -35,12 +33,4 @@ root.render(
   </ConfigProvider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
