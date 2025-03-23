1fa1d0c828a22abe3f20c9aad0f397a0c2323d68
latapac <latashree@gmail.com>
2025-03-23 11:26:39 +0530

    deleted uncessary

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -2,14 +2,12 @@
 
 // project imports
 import MainLayout from 'layout/MainLayout';
-import Loadable from 'ui-component/Loadable';
 import AuditTrail from './Audit_Trial';
 import Dashboard from '../views/dashboard2/Default';
 import OEE from '../views/OEE/Default';
 import Production from '../views/Production/Default';
 import AlarmReport from './Alarm';
 import Protected from './AuthLayout';
-import Audit2 from '../views/Audit2/Default';
 import Active_alarm from '../views/Active_alarm/Default';
 import Batch_details from '../views/batch_details';
 
@@ -17,13 +15,6 @@ import Batch_details from '../views/batch_details';
 // dashboard routing
 import DashboardDefault from 'views/dashboard/Default'
 
-// utilities routing
-import UtilsTypography from 'views/utilities/Typography'
-import UtilsColor from 'views/utilities/Color'
-import UtilsShadow from 'views/utilities/Shadow'
-
-// sample page routing
-import SamplePage from 'views/sample-page'
 
 // ==============================|| MAIN ROUTING ||============================== //
 
@@ -45,20 +36,12 @@ const MainRoutes = {
       ]
     },
     {
-      path: 'typography',
-      element: <UtilsTypography />
-    },
-    {
       path: 'batch',
       element: <Batch_details />
     },
     {
       path: 'OEE',
       element: <OEE />
-    },
-    {
-      path: 'Audit2',
-      element: <Audit2 />
     },
     {
       path: 'Active_alarm',
@@ -75,18 +58,6 @@ const MainRoutes = {
     {
       path: 'dash',
       element: <Dashboard />
-    },
-    {
-      path: 'color',
-      element: <UtilsColor />
-    },
-    {
-      path: 'shadow',
-      element: <UtilsShadow />
-    },
-    {
-      path: '/sample-page',
-      element: <SamplePage />
     },
     {
       path: '/alarm',
