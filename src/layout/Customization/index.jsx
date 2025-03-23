98995eeee372629bceb47b869cdca6907583745a
latapac <latashree@gmail.com>
2025-03-23 11:50:46 +0530

    rm uncessary import

--- src/layout/Customization/index.jsx
+++ src/layout/Customization/index.jsx
@@ -1,12 +1,8 @@
 import { useState } from 'react';
 
-// material-ui
-import { useTheme } from '@mui/material/styles';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
-import Fab from '@mui/material/Fab';
 import Grid from '@mui/material/Grid2';
-import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 
 // third party
@@ -16,14 +12,7 @@ import PerfectScrollbar from 'react-perfect-scrollbar';
 import FontFamily from './FontFamily';
 import BorderRadius from './BorderRadius';
 
-import AnimateButton from 'ui-component/extended/AnimateButton';
-
-// assets
-import { IconSettings } from '@tabler/icons-react';
-// ==============================|| LIVE CUSTOMIZATION ||============================== //
-
 export default function Customization() {
-  const theme = useTheme();
 
   // drawer on/off
   const [open, setOpen] = useState(false);
